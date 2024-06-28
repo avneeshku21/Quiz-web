9032a7cbaea8efa4a86d10f4d5e0b035f8e0b26e
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-28 12:09:51 +0530

    update

--- script.js
+++ script.js
@@ -1,162 +1,268 @@
-let timeLeft = document.querySelector(".time-left");
-let quizContainer = document.getElementById("container");
-let nextBtn = document.getElementById("next-button");
-let countofQuestion = document.querySelector(".number-of-question");
-let displayContainer = document.getElementById("display-conatainer");
-let scoreContainer = document.querySelector(".score-conatainer");
-let restart = document.getElementById("restart");
-let userScore = document.getElementById("user-score");
-let startScreen = document.querySelector("start-screen");
-let startButton = document.getElementById("start-button");
-let questionCount;
-let scoreCount = 0;
-let count = 11;
-let countdown;
+let questions = [
+    {
+        prompt: `Inside which HTML 
+                 element do we put 
+                 the JavaScript?`,
+        options: [
+            "<javascript>",
+            "<js>",
+            "<script>",
+            "<scripting>",
+        ],
+        answer: "<script>",
+    },
 
-const quizArray = [
-  {
-    id: "1",
-    question: "Full form of Html?",
-    options: [
-      "Hello to my world",
-      "Hey text my language",
-      "Hyper text Markup language",
-      "Hyper text Makeup language",
-    ],
-    correct: "answer3",
-  },
-  {
-    id: "2",
-    question: "why do we use ReactJs?",
-    options: [
-      "For building UI's",
-      "For back-end",
-      "For data-bases",
-      "It's nothing.",
-    ],
-    correct: "answer1",
-  },
-  {
-    id: "3",
-    question: "Which tag is incorrect?",
-    option1: ["<kbd>", "<html>", "<abbr>", "All are correct"],
-    correct: "answer4",
-  },
-  {
-    id: "4",
-    question: "which is wrong here?",
-    options: ["var x = 10;", "x = 10", "let x = 10;", "const x = 10;"],
-    correct: "answer2",
-  },
-  {
-    id: "5",
-    question: "What is git?",
-    options: [
-      "version control system",
-      "A ugly thing",
-      "Distributed version control system",
-      "A useful stuff",
-    ],
-    correct: "answer3",
-  },
-  {
-    id: "6",
-    question: "Which is correct?",
-    options: [
-      "<html><body>...</body></html>",
-      "<body><html>...</html></body>",
-      "<script><head>...</head></script>",
-      "All are correct.",
-    ],
-    correct: "answer1",
-  },
-  {
-    id: "7",
-    question: "Which is unchangeable",
-    options: [
-      "var x = 10;",
-      "let x = 10;",
-      "const x = 10;",
-      "All are unchangeable.",
-    ],
-    correct: "answer3",
-  },
-  {
-    id: "8",
-    question: "How many kinds of loops are here in js?",
-    options: ["2", "6", "1", "5"],
-    correct: "answer4",
-  },
-  {
-    id: "9",
-    question: "What does an array called in js?",
-    options: ["Array", "Object", "String", "Boolean"],
-    correct: "answer2",
-  },
-  {
-    id: "10",
-    question: "Which is the best js Library?",
-    options: ["ReactJs", "JQuery", "Angular", "Vue"],
-    correct: "answer1",
-  },
+    {
+        prompt: `How do you call a
+                 function named 
+                 myFunction?`,
+        options: [
+            "call myFunction()",
+            "myFunction()",
+            "call function myFunction",
+            "Call.myFunction",
+        ],
+        answer: "myFunction()",
+    },
+
+    {
+        prompt: `How does a for loop
+                 start?`,
+        options: [
+            "for (i = 0; i <= 5; i++)",
+            "for (i = 0; i <= 5)",
+            "for i = 1 to 5",
+            " for (i <= 5; i++)",
+        ],
+        answer: "for (i = 0; i <= 5; i++)",
+    },
+
+    {
+        prompt: `In JavaScript, which 
+                 of the following is 
+                 a logical operator?`,
+        options: ["|", "&&", "%", "/"],
+        answer: "&&",
+    },
+
+    {
+        prompt: `A named element in a 
+                 JavaScript program that
+                 is used to store and 
+                 retrieve data is a _____.`,
+        options: [
+            "method",
+            "assignment operator",
+            "letiable",
+            "string",
+        ],
+        answer: "letiable",
+    },
 ];
-restart.addEventListener("click", () => {
-  initial();
-  displayContainer.classList.remove("hide");
-  scoreContainer.classList.add("hide");
-});
-nextBtn.addEventListener(
-  "click",
-  (displayNext = () => {
-    questionCount += 1;
-    if (questionCount == quizArray.length) {
-      displayContainer.classList.add("hide");
-      scoreContainer.classList.remove("hide");
-      userScore.innerHTML =
-        "Your Score is" + scoreCount + "out of " + questionCount;
-    } else {
-      countofQuestion.innerHTML =
-        questionCount + 1 + "of" + quizArray.length + "Question";
-      quizeDisplay(questionCount);
-      count = 11;
-      clearInterval(countdown);
-      timerDisplay();
-    }
-  })
+
+// Get Dom Elements
+
+let questionsEl =
+    document.querySelector(
+        "#questions"
+    );
+let timerEl =
+    document.querySelector("#timer");
+let choicesEl =
+    document.querySelector("#options");
+let submitBtn = document.querySelector(
+    "#submit-score"
 );
+let startBtn =
+    document.querySelector("#start");
+let nameEl =
+    document.querySelector("#name");
+let feedbackEl = document.querySelector(
+    "#feedback"
+);
+let reStartBtn =
+    document.querySelector("#restart");
 
-const timerDisplay = () => {
-  countdown = setInterval(() => {
-    count--;
-    timeLeft.innerHTML = `${count}s`;
-    if (count == 0) {
-      clearInterval(countdown);
-      displayNext();
-    }
-  }, 1000);
-};
+// Quiz's initial state
+let currentQuestionIndex = 0;
+let time = questions.length * 15;
+let timerId;
 
-const quizeDisplay = (questionCount) => {
-  let quizCards = document.querySelectorAll(".container-mid");
-  quizCards.forEach((card) => {
-    card.classList.add("hide");
-  });
-  quizCards[questionCount].classList.remove("hide");
-};
+// Start quiz and hide frontpage
 
-function quizCreater() {
-  quizArray.sort(() => Math.random() - 0.5);
-  let div = document.createElement("p");
-  question_DIV.classList.add("question");
-  question_DIV.innerHTML = i.question;
-  div.appendChild(question_DIV);
-  
-  div.innerHTML += `
-    <button class="option-div" onclick="checker(this)"${i.options[0]}</button>
-     <button class="option-div" onclick="checker(this)"${i.options[1]}</button>
-      <button class="option-div" onclick="checker(this)"${i.options[2]}</button>
-      <button class="option-div" onclick="checker(this)"${i.options[3]}</button>
-    questionCount
-
-    `;
+function quizStart() {
+    timerId = setInterval(
+        clockTick,
+        1000
+    );
+    timerEl.textContent = time;
+    let landingScreenEl =
+        document.getElementById(
+            "start-screen"
+        );
+    landingScreenEl.setAttribute(
+        "class",
+        "hide"
+    );
+    questionsEl.removeAttribute(
+        "class"
+    );
+    getQuestion();
 }
+
+// Loop through array of questions and
+// Answers and create list with buttons
+function getQuestion() {
+    let currentQuestion =
+        questions[currentQuestionIndex];
+    let promptEl =
+        document.getElementById(
+            "question-words"
+        );
+    promptEl.textContent =
+        currentQuestion.prompt;
+    choicesEl.innerHTML = "";
+    currentQuestion.options.forEach(
+        function (choice, i) {
+            let choiceBtn =
+                document.createElement(
+                    "button"
+                );
+            choiceBtn.setAttribute(
+                "value",
+                choice
+            );
+            choiceBtn.textContent =
+                i + 1 + ". " + choice;
+            choiceBtn.onclick =
+                questionClick;
+            choicesEl.appendChild(
+                choiceBtn
+            );
+        }
+    );
+}
+
+// Check for right answers and deduct
+// Time for wrong answer, go to next question
+
+function questionClick() {
+    if (
+        this.value !==
+        questions[currentQuestionIndex]
+            .answer
+    ) {
+        time -= 10;
+        if (time < 0) {
+            time = 0;
+        }
+        timerEl.textContent = time;
+        feedbackEl.textContent = `Wrong! The correct answer was 
+        ${questions[currentQuestionIndex].answer}.`;
+        feedbackEl.style.color = "red";
+    } else {
+        feedbackEl.textContent =
+            "Correct!";
+        feedbackEl.style.color =
+            "green";
+    }
+    feedbackEl.setAttribute(
+        "class",
+        "feedback"
+    );
+    setTimeout(function () {
+        feedbackEl.setAttribute(
+            "class",
+            "feedback hide"
+        );
+    }, 2000);
+    currentQuestionIndex++;
+    if (
+        currentQuestionIndex ===
+        questions.length
+    ) {
+        quizEnd();
+    } else {
+        getQuestion();
+    }
+}
+
+// End quiz by hiding questions,
+// Stop timer and show final score
+
+function quizEnd() {
+    clearInterval(timerId);
+    let endScreenEl =
+        document.getElementById(
+            "quiz-end"
+        );
+    endScreenEl.removeAttribute(
+        "class"
+    );
+    let finalScoreEl =
+        document.getElementById(
+            "score-final"
+        );
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute(
+        "class",
+        "hide"
+    );
+}
+
+// End quiz if timer reaches 0
+
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+    if (time <= 0) {
+        quizEnd();
+    }
+}
+
+// Save score in local storage
+// Along with users' name
+
+function saveHighscore() {
+    let name = nameEl.value.trim();
+    if (name !== "") {
+        let highscores =
+            JSON.parse(
+                window.localStorage.getItem(
+                    "highscores"
+                )
+            ) || [];
+        let newScore = {
+            score: time,
+            name: name,
+        };
+        highscores.push(newScore);
+        window.localStorage.setItem(
+            "highscores",
+            JSON.stringify(highscores)
+        );
+        alert(
+            "Your Score has been Submitted"
+        );
+    }
+}
+
+// Save users' score after pressing enter
+
+function checkForEnter(event) {
+    if (event.key === "Enter") {
+        saveHighscore();
+        alert(
+            "Your Score has been Submitted"
+        );
+    }
+}
+nameEl.onkeyup = checkForEnter;
+
+// Save users' score after clicking submit
+
+submitBtn.onclick = saveHighscore;
+
+// Start quiz after clicking start quiz
+
+startBtn.onclick = quizStart;
